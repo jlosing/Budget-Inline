e27c805f1dc8882d96341cb45a07348ac443f9ee
jlosing <jwlosing@gmail.com>
2025-05-06 15:31:56 -0500

    aded user service

--- src/app/budget.service.ts
+++ src/app/budget.service.ts
@@ -1,7 +1,13 @@
 import { inject, Injectable } from '@angular/core';
-import { Firestore, collection, collectionData, query, orderBy } from '@angular/fire/firestore';
+import { Firestore, 
+  collection, 
+  collectionData, 
+  limit, 
+  query, 
+  where } from '@angular/fire/firestore';
+import { map, Observable } from 'rxjs';
 
-export interface budget {
+export interface Budget {
   budgetId?: string,
   uid: string,
   name: string,
@@ -15,7 +21,22 @@ export interface budget {
 export class BudgetService {
   private firestore = inject(Firestore);
 
-  getUserBudget(uid: string) {
+  getUserBudget(uid: string): Observable<Budget | null> {
     const budgetRef = collection(this.firestore, 'budget');
+    const budgetQuery = query(
+      budgetRef, 
+      where('uid', '==', uid),
+      limit(1),
+    );
+
+    return (collectionData(budgetQuery, { idField: 'budgetId'}) as Observable<Budget[]>).pipe(
+      map(budgets => {
+      if (budgets.length > 0) {
+          console.log(`Budget found for user ${uid}:`, budgets[0]);
+          return budgets[0];
+        }
+        else return null;
+  })
+    );
   }
 }
