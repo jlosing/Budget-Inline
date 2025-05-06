28158f99e644d9ba283fd9db5b8e9ef085741638
jlosing <jwlosing@gmail.com>
2025-05-06 15:11:36 -0500

    sdf
    B
    :x
    B
    B
    B
    B
    B
    A
    A
    A
    A
    A
    A
    A
    A
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    B
    
    B
    B
    B

--- src/app/budget.service.ts
+++ src/app/budget.service.ts
@@ -1,11 +1,21 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import { Firestore, collection, collectionData, query, orderBy } from '@angular/fire/firestore';
 
-
+export interface budget {
+  budgetId?: string,
+  uid: string,
+  name: string,
+  amount: number,
+  categories: { [categoryName: string]: number };
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class BudgetService {
+  private firestore = inject(Firestore);
 
-  constructor() { }
+  getUserBudget(uid: string) {
+    const budgetRef = collection(this.firestore, 'budget');
+  }
 }
