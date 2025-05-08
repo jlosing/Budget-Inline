bf2a9541396a3403acb67da4cda0f38fb2eeffee
jauntee <noahwj305@gmail.com>
2025-05-08 15:28:57 -0500

    Moved navbar to the side, added a dollar amount option to add finanaces

---
+++ src/app/debt-tracker/debt-tracker.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-debt-tracker',
+  imports: [],
+  templateUrl: './debt-tracker.component.html',
+  styleUrl: './debt-tracker.component.css'
+})
+export class DebtTrackerComponent {
+
+}
